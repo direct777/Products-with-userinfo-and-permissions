9435c85db2dad42f7ba48a314f82d8a495820530
Alexander Nikandrov <sportlake@mail.ru>
2020-11-14 11:12:14 +0200

    Cluster support is added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 /* eslint-disable no-console */
-const logger = require('./logger');
-const app = require('./app');
-const port = app.get('port');
-const server = app.listen(port);
+const cluster = require('cluster');
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
+const CLUSTER_COUNT = 4;
+if (cluster.isMaster) {
+  for (let i = 0; i < CLUSTER_COUNT; i++) {
+    cluster.fork();
+  }
+} else {
+  const logger = require('./logger');
+  const app = require('./app');
+  const port = app.get('port');
+  const server = app.listen(port);
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+  process.on('unhandledRejection', (reason, p) =>
+    logger.error('Unhandled Rejection at: Promise ', p, reason)
+  );
+
+  server.on('listening', () =>
+    logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+  );
+}
